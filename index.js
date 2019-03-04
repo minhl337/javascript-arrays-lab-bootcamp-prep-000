9e24945d3f305f4b7daeb7b9c433d59de7f265fa
minhl337 <imnotminh@gmail.com>
2019-03-04 21:41:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,46 +1,40 @@
-// var kittens = ["Milo", "Otis", "Garfield"] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
-// // Add your functions and code here
-// function destructivelyAppendKitten(x){
-//   kittens.push(x);
-//   return kittens
-// }
-
-// function destructivelyPrependKitten(x){
-//   kittens.unshift(x)
-//   return kittens
-// }
-
-// function destructivelyRemoveLastKitten(){
-//   kittens.pop()
-//   return kittens
-// }
-
-// function destructivelyRemoveFirstKitten(){
-//   kittens.shift()
-//   return kittens
-// }
-
-// function appendKitten(x){
-//   return kittens.concat(x)
-// }
-
-// function prependKitten(x){
-//   return [x,...kittens]
-// }
-
-// function removeLastKitten(){
-//   return kittens.slice(0,kittens.length-1)
-// }
-
-// function removeFirstKitten(){
-//   return kittens.slice(1)
-// }
-
-var kittens = ["Milo", "Otis", "Garfield"];
-
-function destructivelyAppendKitten(y){
-  kittens.push(y);
-  return kittens;
+// Add your functions and code here
+function destructivelyAppendKitten(x){
+  kittens.push(x);
+  return kittens
 }
+
+function destructivelyPrependKitten(x){
+  kittens.unshift(x)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(x){
+  return kittens.concat(x)
+}
+
+function prependKitten(x){
+  return [x,...kittens]
+}
+
+function removeLastKitten(){
+  return kittens.slice(0,kittens.length-1)
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
+
 
